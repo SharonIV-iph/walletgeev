256cacd5f93902bd24a00e2a09b4e0996e7b357f
Your Name <you@example.com>
2025-04-23 14:50:55 +0330

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+/**
+ * The main page component that renders the HomePage component.
+ *
+ * @returns {JSX.Element} The rendered HomePage component.
+ */
+const Page = () => {
+    return <>hello</>;
+};
+
+export default Page;
